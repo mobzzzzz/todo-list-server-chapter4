9c61097b7078c26809f0350893836c3a6ee24afb
Jaewon Noh <opqw987@gmail.com>
2024-05-14 11:35:42 +0900

    Feat: SwaggerConfig.kt 추가

---
+++ src/main/kotlin/org/example/todolistserverchapter3/api/v1/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package org.example.todolistserverchapter3.api.v1.infra.swagger
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(
+            Info()
+                .title("Todo API")
+                .description("Todo  API schema")
+                .version("1.0.0")
+        )
+}

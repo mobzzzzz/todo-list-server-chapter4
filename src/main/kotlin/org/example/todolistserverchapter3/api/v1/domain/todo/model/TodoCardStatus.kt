a8e8aa1e4c4f613a938315d5042e7768ef4ef5b0
Jaewon Noh <opqw987@gmail.com>
2024-05-16 14:14:03 +0900

    Chore: Enum name CamelCase로 수정

--- src/main/kotlin/org/example/todolistserverchapter3/api/v1/domain/todo/model/TodoCardStatus.kt
+++ src/main/kotlin/org/example/todolistserverchapter3/api/v1/domain/todo/model/TodoCardStatus.kt
@@ -1,7 +1,7 @@
 package org.example.todolistserverchapter3.api.v1.domain.todo.model
 
 enum class TodoCardStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    COMPLETED
+    NotStarted,
+    InProgress,
+    Completed
 }

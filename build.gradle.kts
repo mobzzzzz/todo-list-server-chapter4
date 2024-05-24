fbfd6f516ae5eac36c66c4acfac29c8be3e34a6b
Jaewon Noh <opqw987@gmail.com>
2024-05-24 11:44:33 +0900

    Build: Spring security 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ allOpen {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")

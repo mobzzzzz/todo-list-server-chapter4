e18f9dd053374f04afbc036841ed1768692a3b5e
Jaewon Noh <opqw987@gmail.com>
2024-05-24 12:03:33 +0900

    Feat: User service 패스워드 암호화, 매칭 추가

--- src/main/kotlin/org/example/todolistserverchapter3/api/v1/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/org/example/todolistserverchapter3/api/v1/domain/user/service/UserServiceImpl.kt
@@ -11,23 +11,38 @@ import org.example.todolistserverchapter3.api.v1.domain.user.repository.UserRepo
 import org.example.todolistserverchapter3.api.v1.exception.NoPermissionException
 import org.example.todolistserverchapter3.api.v1.util.DtoConverter
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserServiceImpl(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) : UserService {
 
     @Transactional
     override fun signUp(request: SignUpDto): UserDto {
-        return DtoConverter.convertToUserDto(userRepository.save(User.fromDto(request)))
+        val encryptedPassword = passwordEncoder.encode(request.password)
+        return DtoConverter.convertToUserDto(
+            userRepository.save(
+                User.fromDto(
+                    request.copy(password = encryptedPassword)
+                )
+            )
+        )
     }
 
     @Transactional
     override fun signIn(request: SignInDto): UserDto {
         val user = userRepository.findByEmailAndPassword(request.email, request.password)
             ?: throw IllegalStateException("User not found with email")
+
+        if (!passwordEncoder.matches(
+                request.password,
+                user.password
+            )
+        ) throw IllegalStateException("Password is incorrect")
 
         return DtoConverter.convertToUserDto(user)
     }
@@ -45,7 +60,10 @@ class UserServiceImpl(
     @Transactional
     override fun updateUserProfile(userId: Long, currentUserId: Long, request: UserUpdateProfileDto): UserDto {
         val user = userRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("User not found", userId)
-        val currentUser = userRepository.findByIdOrNull(currentUserId) ?: throw ModelNotFoundException("User not found", currentUserId)
+        val currentUser = userRepository.findByIdOrNull(currentUserId) ?: throw ModelNotFoundException(
+            "User not found",
+            currentUserId
+        )
 
         if (!user.hasPermission(currentUser)) throw NoPermissionException()
 
@@ -57,7 +75,10 @@ class UserServiceImpl(
     @Transactional
     override fun deactivate(userId: Long, currentUserId: Long) {
         val user = userRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("User not found", userId)
-        val currentUser = userRepository.findByIdOrNull(currentUserId) ?: throw ModelNotFoundException("User not found", currentUserId)
+        val currentUser = userRepository.findByIdOrNull(currentUserId) ?: throw ModelNotFoundException(
+            "User not found",
+            currentUserId
+        )
 
         if (!user.hasPermission(currentUser)) throw NoPermissionException()
 

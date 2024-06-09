1251d8cf4fb75f335b1e6903802dad6ef8d81291
Jaewon Noh <opqw987@gmail.com>
2024-06-09 15:14:45 +0900

    Build: Kotest, Mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     kotlin("plugin.noarg") version "1.9.23"
+    kotlin("kapt") version "1.9.23"
 }
 
 group = "org.example"
@@ -37,6 +38,9 @@ allOpen {
     annotation("jakarta.persistence.Embeddable")
 }
 
+val kotestVersion = "5.5.5"
+val mockkVersion = "1.13.8"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -50,6 +54,10 @@ dependencies {
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
 
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
